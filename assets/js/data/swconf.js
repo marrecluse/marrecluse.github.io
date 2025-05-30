3576b3c3c88206f32d41637155ebb61dfcdc208b
M Abdul Rehman <danijakhar11@gmail.com>
2025-05-30 11:59:13 +0100

    docs(about): site updates

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748473344',resources: [
+    cacheName: 'chirpy-1748602380',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
